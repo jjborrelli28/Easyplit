2c95606e76ef2a0fe1463298264dff37c6d78c3c
jjborrelli28 <juanjose.borrelli@sugaway.com>
2025-05-05 00:02:59 -0300

    create middleware auth check

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { type NextRequest, NextResponse } from 'next/server';
+
+import { verifyToken } from './lib/auth';
+
+export function middleware(req: NextRequest) {
+    const token = req.cookies.get('token')?.value;
+    if (!token) return NextResponse.redirect(new URL('/login', req.url));
+
+    try {
+        verifyToken(token);
+        return NextResponse.next();
+    } catch {
+        return NextResponse.redirect(new URL('/login', req.url));
+    }
+}
+
+export const config = {
+    matcher: ['/dashboard'],
+};
