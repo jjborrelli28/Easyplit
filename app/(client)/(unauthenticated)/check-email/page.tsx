7d72cee979ce5fb956ae45c253aa64a62ec9b6fb
jjborrelli28 <juanjose.borrelli@sugaway.com>
2025-05-18 00:49:10 -0300

    modify check-mail page

--- app/(client)/(unauthenticated)/check-email/page.tsx
+++ app/(client)/(unauthenticated)/check-email/page.tsx
@@ -4,30 +4,25 @@ import { MailCheck } from "lucide-react";
 
 import Button from "@/components/Button";
 import PageContainer from "@/components/PageContainer";
+import MessageCard from "@/components/MessageCard";
 
 const CheckEmailPage = () => {
   return (
     <PageContainer centered>
-      <div className="border-h-background w-full max-w-md space-y-8 border p-8 text-center shadow-xl">
-        <MailCheck className="text-primary mx-auto h-12 w-12" />
-
-        <h1 className="text-3xl font-semibold">Revisá tu correo</h1>
-
+      <MessageCard
+        icon={MailCheck}
+        title="Revisá tu correo"
+        actionLabel="Volver al inicio de sesión"
+        actionHref="/login"
+      >
         <div className="space-y-4">
-          <p className="text-foreground/75 text-md">
+          <p>
             Si existe una cuenta con ese email, vas a recibir un enlace para
             restablecer tu contraseña.
           </p>
-
-          <p className="text-foreground/75 text-sm">
-            Recordá revisar también la carpeta de spam o promociones.
-          </p>
+          <p>Recordá revisar también la carpeta de spam o promociones.</p>
         </div>
-
-        <Button href="/login" fullWidth>
-          Volver al inicio de sesión
-        </Button>
-      </div>
+      </MessageCard>
     </PageContainer>
   );
 };
