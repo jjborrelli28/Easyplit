3fa9e8546da856e7e87eb7119cd995f752d3d18a
jjborrelli28 <juanjose.borrelli@sugaway.com>
2025-05-27 17:22:24 -0300

    minor fix to ToggleTheme

--- components/ThemeToggle/index.tsx
+++ components/ThemeToggle/index.tsx
@@ -11,12 +11,11 @@ const ThemeToggle = () => {
 
   const [isMounted, setIsMounted] = useState(false);
   const [isChanging, setIsChanging] = useState(false);
+  const [hasInteracted, setHasInteracted] = useState(false);
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
-  if (!isMounted) return null;
 
   const isDark = resolvedTheme === "dark";
 
@@ -25,6 +24,7 @@ const ThemeToggle = () => {
 
     setTheme(isDark ? "light" : "dark");
     setIsChanging(true);
+    setHasInteracted(true);
 
     setTimeout(() => {
       setIsChanging(false);
@@ -37,24 +37,29 @@ const ThemeToggle = () => {
       aria-label="Change theme"
       className="group bg-h-background border-primary relative flex h-8 w-16 cursor-pointer items-center rounded-full border px-1 transition-colors duration-300"
     >
-      <div
-        className={clsx(
-          "absolute right-1 left-1 flex h-6 w-[calc(100%_-_8px] rounded-full",
-          isDark
-            ? "animate-justify-start-to-end"
-            : "animate-justify-end-to-start",
-        )}
-      >
-        <span
+      {isMounted && (
+        <div
           className={clsx(
-            "bg-primary group-hover:bg-primary/90 z-10 h-6 w-6 rounded-full shadow-md transition-colors duration-300 ease-in-out",
-            isChanging && "animate-width-pulse",
+            "absolute right-1 left-1 flex h-6 w-[calc(100%_-_8px)] rounded-full",
+            isDark ? "justify-end" : "justify-start",
+            hasInteracted &&
+              (isDark
+                ? "animate-justify-start-to-end"
+                : "animate-justify-end-to-start"),
           )}
-        />
-      </div>
+        >
+          <span
+            className={clsx(
+              "bg-primary group-hover:bg-primary/90 z-10 h-6 w-6 rounded-full shadow-md transition-colors duration-300 ease-in-out",
+              isChanging && "animate-width-pulse",
+            )}
+          />
+        </div>
+      )}
 
       <div className="relative z-20 flex w-full justify-between px-1">
         <Sun size={16} className="text-white" />
+
         <Moon size={16} className="text-black" />
       </div>
     </button>
